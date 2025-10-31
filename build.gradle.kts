c3dc62792aa85818fc3bc9b66eb4da7d4106c3e4
Márton Braun <marton.braun@jetbrains.com>
2025-10-31 08:56:38 +0000

    Remove kotlin android plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.composeHotReload) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
+//    alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
 }
