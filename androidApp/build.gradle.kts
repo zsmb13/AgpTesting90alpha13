721cd5033ed12b971ce92446f573d2751ffcff04
Márton Braun <marton.braun@jetbrains.com>
2025-11-04 15:16:29 +0100

    Use built-in Kotlin plugin for AGP 9

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,7 +1,6 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlinAndroid)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
